b3ce5e8fc532d4f874504bc3d6b17cb2771fa7ae
Kishalay Das <kishalaydas@Kishalays-MacBook-Pro.local>
2025-06-10 22:35:08 +0530

    push from local

--- eval_utils.py
+++ eval_utils.py
@@ -106,22 +106,25 @@ def get_crystals_list(frac_coords, atom_types, lengths, angles, num_atoms):
 
 
 def smact_validity(comp, count,use_pauling_test=True,include_alloys=True):
-    # s=[]
-    #
-    # for elem in comp:
-    #     try:
-    #         if elem<0:
-    #             elem=0
-    #         chem_symbol = chemical_symbols[elem]
-    #         s.append(chem_symbol)
-    #     except Exception:
-    #         continue
-    # elem_symbols = tuple(s)
+    s=[]
+
+    for elem in comp:
+        try:
+            if elem<0:
+                elem=0
+            chem_symbol = chemical_symbols[elem]
+            s.append(chem_symbol)
+        except Exception:
+            continue
+    elem_symbols = tuple(s)
+
     # try:
     #     elem_symbols = tuple([chemical_symbols[elem] for elem in comp])
     # except Exception:
     #     print(comp)
-    elem_symbols = tuple([chemical_symbols[elem] for elem in comp])
+
+    # elem_symbols = tuple([chemical_symbols[elem] for elem in comp])
+    
     space = smact.element_dictionary(elem_symbols)
     smact_elems = [e[1] for e in space.items()]
     electronegs = [e.pauling_eneg for e in smact_elems]
