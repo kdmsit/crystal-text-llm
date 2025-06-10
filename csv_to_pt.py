162068de25a1addc61b9214619498ce229d5456f
Kishalay Das <kishalaydas@Kishalays-MacBook-Pro.local>
2025-06-10 11:55:47 +0530

    push from local

--- csv_to_pt.py
+++ csv_to_pt.py
@@ -14,7 +14,7 @@ def main(args):
 
     n_atom, x_coord, a_type, length, angle = [], [], [], [], []
     df_data = pd.read_csv(data_path)
-    pbar = tqdm(total=df_data.size[0], desc="Generating Samples")
+    pbar = tqdm(total=len(df_data), desc="Generating Samples")
     for index, row in df_data.iterrows():
         cif_str = row['gen_str']
         frac_coords, atom_types, lengths, angles, num_atoms = process_one(cif_str, True, False,'crystalnn', False, 0.01)
