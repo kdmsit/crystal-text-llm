a21381263e355899088a0f698394a0e2676e703f
Kishalay Das <kishalaydas@Kishalays-MacBook-Pro.local>
2025-06-10 23:09:49 +0530

    push from local

--- eval_utils.py
+++ eval_utils.py
@@ -90,13 +90,13 @@ def get_crystals_list(frac_coords, atom_types, lengths, angles, num_atoms):
     for batch_idx, num_atom in enumerate(num_atoms.tolist()):
         cur_frac_coords = frac_coords.narrow(0, start_idx, num_atom)
         cur_atom_types = atom_types.narrow(0, start_idx, num_atom)
-        print(cur_atom_types)
+        # print(cur_atom_types)
         cur_atom_types = torch.where(
             (cur_atom_types < 0) | (cur_atom_types > 118),
-            torch.tensor(0, dtype=cur_atom_types.dtype),
+            torch.tensor(7, dtype=cur_atom_types.dtype),
             cur_atom_types
         )
-        print(cur_atom_types)
+        # print(cur_atom_types)
         cur_lengths = lengths[batch_idx]
         cur_angles = angles[batch_idx]
 
