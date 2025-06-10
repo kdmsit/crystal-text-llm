8898809f7b89dab27ce17ccef2559c99695fecf8
Kishalay Das <kishalaydas@Kishalays-MacBook-Pro.local>
2025-06-10 22:39:15 +0530

    push from local

--- eval_utils.py
+++ eval_utils.py
@@ -125,6 +125,7 @@ def smact_validity(comp, count,use_pauling_test=True,include_alloys=True):
 
     # elem_symbols = tuple([chemical_symbols[elem] for elem in comp])
 
+    print(elem_symbols[205])
     space = smact.element_dictionary(elem_symbols)
     smact_elems = [e[1] for e in space.items()]
     electronegs = [e.pauling_eneg for e in smact_elems]
