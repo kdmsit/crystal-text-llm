7044801294a95270856f35dbabc288d0b9227a7b
Kishalay Das <kishalaydas@Kishalays-MacBook-Pro.local>
2025-06-10 13:36:27 +0530

    push from local

--- csv_to_pt.py
+++ csv_to_pt.py
@@ -21,8 +21,8 @@ def main(args):
 
     pbar = tqdm(total=len(df_data), desc="Generating Samples")
     for index, row in df_data.iterrows():
-        print(index)
-        if index == 62:
+        # print(index)
+        if index == 62 or 63:
             continue
         cif_str = str(row['cif'])
         # future = executor.submit(task, cif_str)
