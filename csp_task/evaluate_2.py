2d608d5914ad3087fc2c5429c5c2afe9fa9fa53a
Kishalay Das <kishalaydas@Kishalays-MacBook-Pro.local>
2025-07-11 11:22:42 +0530

    push from local

--- csp_task/evaluate_2.py
+++ csp_task/evaluate_2.py
@@ -116,7 +116,7 @@ def generation(loader, model, num_evals, step_lr = 1e-5, diff_steps = 1000):
         atom_types.append(torch.stack(batch_atom_types, dim=0))
         lattices.append(torch.stack(batch_lattices, dim=0))
 
-        input_data_list += [d.to(device) for d in input_data_list]
+        print(input_data_list)
         input_data_list = input_data_list + batch.to_data_list()
 
     frac_coords = torch.cat(frac_coords, dim=1)
